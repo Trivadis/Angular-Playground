332f48af7ee41be328cfe809cee5ebf72d3443ae
Thomas Gassmann <thomas.gassmann@windowslive.com>
2018-02-23 14:29:10 +0100

    Interceptor

--- ByodPortal/src/app/core/core.module.ts
+++ ByodPortal/src/app/core/core.module.ts
@@ -3,18 +3,35 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { throwIfAlreadyLoaded } from './module-import-guard';
 import { AppCustomPreloader } from './app-custom-preloader';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './auth.interceptor';
 
 // https://angular.io/guide/styleguide#core-feature-module
 
 @NgModule({
   imports: [],
   exports: [],
-  declarations: [],
-  providers: [AppCustomPreloader]
+  declarations: []
 })
 export class CoreModule {
-    constructor( @Optional() @SkipSelf() parentModule: CoreModule) {
-        throwIfAlreadyLoaded(parentModule, 'CoreModule');
-      }
-    }
+  constructor(
+    @Optional()
+    @SkipSelf()
+    parentModule: CoreModule
+  ) {
+    throwIfAlreadyLoaded(parentModule, 'CoreModule');
+  }
 
+  static forRoot() {
+    return {
+      ngModule: CoreModule,
+      providers: [AppCustomPreloader,
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: AuthInterceptor,
+          multi: true
+        }
+      ]
+    };
+  }
+}
