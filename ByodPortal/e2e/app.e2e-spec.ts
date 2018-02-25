db468893df584086453f72a3456a0c56df0fbcc7
Thomas Bandixen <thomas@bandixen.ch>
2018-02-25 13:03:36 +0100

    e2e tests

--- ByodPortal/e2e/app.e2e-spec.ts
+++ ByodPortal/e2e/app.e2e-spec.ts
@@ -1,14 +1,31 @@
 import { AppPage } from './app.po';
 
 describe('byod-portal App', () => {
-  let page: AppPage;
+    let page: AppPage;
 
-  beforeEach(() => {
-    page = new AppPage();
-  });
+    beforeEach(() => { page = new AppPage(); });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
-  });
+    it('should display welcome message', () => {
+        page.navigateTo('/');
+        expect(page.getText('app-root h2'))
+            .toEqual('Byod Portal');
+    });
+
+    it('should navigate to byod page', () => {
+        page.navigateTo('/');
+
+        page.getElement('[ng-reflect-router-link="/employees"]')
+            .click();
+
+        expect(page.getCurrentUrl()).toContain('/employees');
+    });
+
+    it('should search correct', () => {
+        page.navigateTo('/employees');
+
+        page.search('Thomas');
+
+        expect(page.getResult().count()).toEqual(3);
+    });
+
 });
--- ByodPortal/e2e/app.po.ts
+++ ByodPortal/e2e/app.po.ts
@@ -1,11 +1,27 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get('/');
-  }
+    navigateTo(url: string) {
+        return browser.get(url);
+    }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
-  }
+    getElement(selector: string) {
+        return element(by.css(selector));
+    }
+
+    getText(selector: string) {
+        return this.getElement(selector).getText();
+    }
+
+    getCurrentUrl() {
+        return browser.getCurrentUrl();
+    }
+
+    search(search: string) {
+        this.getElement('input[type="search"]').sendKeys(search);
+    }
+
+    getResult() {
+        return element.all(by.css('table.table tbody tr'));
+    }
 }
